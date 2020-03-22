0a362cbcfbfa2568f03d3ba1c622647c3e79fba4
seabbs <s.e.abbott12@gmail.com>
2020-03-22 15:59:11 +0000

    towards a regional nowcasting framework
    Former-commit-id: 3e7ce96acf1d7e2923f0862c7e63097fc4835961

---
+++ docs/posts/global/nowcast/nowcasts/singapore_nowcast.R
@@ -0,0 +1,38 @@
+# Get utils ---------------------------------------------------------------
+
+source("utils/rt_pipeline.R")
+
+# Read in linelist --------------------------------------------------------
+linelist <- readxl::read_xlsx("data/singapore.xlsx") %>%
+  dplyr::mutate(import_status =
+           dplyr::if_else(!is.na(Imported), "imported", "local"),
+         ## Clean up onset date based on excel origin
+         date_onset = as.numeric(`Sx onset`) %>%
+           as.Date(origin = "1900-01-01"),
+         date_confirm = as.Date(`Dx date`),
+         ## Estimate report delay - could also use isolation date here.
+         report_delay = as.numeric(date_confirm - date_onset),
+         ## Set negative report delays to 0
+         report_delay = dplyr::if_else(report_delay < 0, 0, report_delay))
+
+# Get WHO sit rep case counts ---------------------------------------------
+
+total_cases <- NCoVUtils::get_who_cases(country = "Singapore", daily = TRUE)
+
+
+# Join imported and local cases -------------------------------------------
+
+cases <- EpiNow::get_local_import_case_counts(total_cases, linelist)
+
+# Run analysis pipeline and save results ----------------------------------
+
+## Set the target date
+target_date <- as.character(max(cases$date))
+
+## Run and save analysis pipeline
+rt_pipeline(
+  cases = cases,
+  linelist = linelist,
+  target_folder = file.path("results/singapore", target_date),
+  target_date = target_date,
+  start_rate_of_spread_est = "2020-01-27")
