cfe1eaf2eb8060ca674d02e9799ccc0d86db42f4
seabbs <s.e.abbott12@gmail.com>
2020-03-23 10:36:26 +0000

    added map to about page
    Former-commit-id: 77bd055190b071a327aa47f7f7d81198845ce913

---
+++ clean_nowcasts.R
@@ -0,0 +1,12 @@
+require(stringr)
+require(magrittr)
+require(purrr)
+
+posts <- list.dirs("docs/posts", recursive = FALSE) %>% 
+  stringr::str_remove("docs/posts/")
+
+
+purrr::walk(posts, 
+            function(post) {
+              unlink(file.path(file.path("docs/posts", post, "nowcast")))
+            })
