59b3942348ffdb5a7675da72801903019755df0e
seabbs <s.e.abbott12@gmail.com>
2020-03-23 11:22:57 +0000

    updated deployment to pages
    Former-commit-id: 761fcd2d9b77a2b16cb550e06373d53741d34f6c

--- bin/update_nowcasts.sh
+++ bin/update_nowcasts.sh
@@ -25,4 +25,6 @@ Rscript utils/clean_nowcasts.R
 git add --all && git commit -m "Updated nowcasts" && git push
 
 ## Deploy the website to gh-pages
-git subtree push --prefix docs origin gh-pages
+git subtree split --squash --prefix docs -b gh-pages
+git push -f origin gh-pages:gh-pages
+git branch -D gh-pages
