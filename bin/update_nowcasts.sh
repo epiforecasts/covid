40c8fc799197d52a5ee8b64ba16d14e04f423622
seabbs <s.e.abbott12@gmail.com>
2020-03-23 10:36:26 +0000

    added map to about page
    Former-commit-id: 48eb65e026b75984fdefdf56d65c9d38062044de

--- bin/update_nowcasts.sh
+++ bin/update_nowcasts.sh
@@ -18,8 +18,11 @@ Rscript utils/update_posts.R
 ## Update the website
 Rscript -e "rmarkdown::render_site()"
 
+## Clean up nowcast folders
+Rscript _utils/clean_nowcasts.R
+
 ## Update the git repo with new results
-git add --all && git commit -m "Updated nowcasts"
+git add --all && git commit -m "Updated nowcasts" && git push
 
 ## Deploy the website to gh-pages
 git subtree push --prefix docs origin gh-pages
