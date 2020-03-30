16ddf182b4f3863881e69c4eeec0508381a04b89
seabbs <s.e.abbott12@gmail.com>
2020-03-30 18:53:30 +0000

    standardised regional breakdown format
    Former-commit-id: cca65caa6c110ba849cb4f835cccca5766716da8

--- _posts/regional-breakdowns/italy/nowcast/update_nowcasts.R
+++ _posts/regional-breakdowns/italy/nowcast/update_nowcasts.R
@@ -24,7 +24,7 @@ region_codes <- cases %>%
   unique() %>% 
   dplyr::mutate(region_code = as.numeric(region_code))
 
-saveRDS(region_codes, "_posts/italy/nowcast/data/region_codes.rds")
+saveRDS(region_codes, "_posts/regional-breakdowns/italy/nowcast/data/region_codes.rds")
 
 cases <- cases %>% 
   dplyr::rename(local = cases) %>% 
@@ -34,15 +34,7 @@ cases <- cases %>%
 
 # Get linelist ------------------------------------------------------------
 
-linelist <- #' Get a combined linelist based on multiple countries data
-    NCoVUtils::get_international_linelist("Germany") %>%
-      dplyr::bind_rows(NCoVUtils::get_international_linelist("Italy")) %>%
-      dplyr::bind_rows(NCoVUtils::get_international_linelist("France")) %>%
-      dplyr::bind_rows(NCoVUtils::get_international_linelist("Spain")) %>%
-      dplyr::bind_rows(NCoVUtils::get_international_linelist("Autria")) %>%
-      dplyr::bind_rows(NCoVUtils::get_international_linelist("Netherlands")) %>%
-      dplyr::bind_rows(NCoVUtils::get_international_linelist("Belgium")) %>%
-      tidyr::drop_na(report_delay)
+linelist <- NCoVUtils::get_international_linelist()
 
 # Set up cores -----------------------------------------------------
 
@@ -56,7 +48,7 @@ EpiNow::regional_rt_pipeline(
   cases = cases, 
   linelist = linelist, 
   regional_delay = FALSE,
-  target_folder = "_posts/italy/nowcast/results", 
+  target_folder = "_posts/regional-breakdowns/italy/nowcast/regional", 
   merge_onsets = FALSE
 )
 
@@ -64,7 +56,7 @@ EpiNow::regional_rt_pipeline(
 # Summarise results -------------------------------------------------------
 
 
-EpiNow::regional_summary(results_dir = "_posts/italy/nowcast/results", 
-                         summary_dir = "_posts/italy/nowcast/summary",
+EpiNow::regional_summary(results_dir = "_posts/regional-breakdowns/italy/nowcast/regional", 
+                         summary_dir = "_posts/regional-breakdowns/italy/nowcast/regional-summary",
                          target_date = "latest",
                          region_scale = "Region")
--- _posts/regional-breakdowns/united-kingdom/nowcast/update_nowcasts.R
+++ _posts/regional-breakdowns/united-kingdom/nowcast/update_nowcasts.R
@@ -23,15 +23,7 @@ cases <- cases %>%
 
 # Get linelist ------------------------------------------------------------
 
-linelist <- #' Get a combined linelist based on multiple countries data
-    NCoVUtils::get_international_linelist("Germany") %>%
-      dplyr::bind_rows(NCoVUtils::get_international_linelist("Italy")) %>%
-      dplyr::bind_rows(NCoVUtils::get_international_linelist("France")) %>%
-      dplyr::bind_rows(NCoVUtils::get_international_linelist("Spain")) %>%
-      dplyr::bind_rows(NCoVUtils::get_international_linelist("Autria")) %>%
-      dplyr::bind_rows(NCoVUtils::get_international_linelist("Netherlands")) %>%
-      dplyr::bind_rows(NCoVUtils::get_international_linelist("Belgium")) %>%
-      tidyr::drop_na(report_delay)
+linelist <-  NCoVUtils::get_international_linelist()
 
 # Set up cores -----------------------------------------------------
 
@@ -45,14 +37,14 @@ EpiNow::regional_rt_pipeline(
   cases = cases, 
   linelist = linelist, 
   regional_delay = FALSE,
-  target_folder = "_posts/united-kingdom/nowcast/results", 
+  target_folder = "_posts/regional-breakdowns/united-kingdom/nowcast/regional", 
   merge_onsets = FALSE
 )
 
 
 # Summarise results -------------------------------------------------------
 
-EpiNow::regional_summary(results_dir = "_posts/united-kingdom/nowcast/results", 
-                         summary_dir = "_posts/united-kingdom/nowcast/summary",
+EpiNow::regional_summary(results_dir = "_posts/regional-breakdowns/united-kingdom/nowcast/regional", 
+                         summary_dir = "_posts/regional-breakdowns/united-kingdom/nowcast/regional-summary",
                          target_date = "latest",
                          region_scale = "Region")
