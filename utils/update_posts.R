27132369bf3980560602e21cf69950ce7a0cb292
seabbs <s.e.abbott12@gmail.com>
2020-03-30 22:38:41 +0000

    auto generation of pages for country reports
    Former-commit-id: 3956df923666837fdbd680ba8b3923e1ddb8b1f1

--- utils/update_posts.R
+++ utils/update_posts.R
@@ -1,17 +1,19 @@
 require(stringr)
 require(magrittr)
-require(furrr)
 require(future)
+require(furrr)
 
-posts <- c("_posts/global" , list.dirs("_posts/regional-breakdowns", recursive = FALSE))
+posts <- c("_posts/global" , list.dirs("_posts/regional-breakdowns", recursive = FALSE),
+           list.dirs("_posts/national-summary", recursive = FALSE))
 
 
 future::plan("multiprocess")
 
-rendered_posts <- furrr::future_map(posts, 
+rendered_output <- furrr::future_map(posts, 
             function(post) {
               post_name <- stringr::str_split(post, "/")[[1]] %>% 
                 dplyr::last()
               file.copy("library.bib", file.path(post), overwrite = TRUE)
-              rmarkdown::render(file.path(post, paste0(post_name, ".Rmd")))
+              
+              rmarkdown::render(file.path(post, paste0(post_name, ".Rmd")), quiet = FALSE)
             }, .progress = TRUE)
