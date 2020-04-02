b247a6a7e9a7e66064994eca5767d2cbc122a007
seabbs <s.e.abbott12@gmail.com>
2020-04-02 16:04:27 +0000

    update figure sizes and captions for maps

---
+++ docs/posts/national/united-kingdom/nowcast/update_nowcasts.R
@@ -0,0 +1,51 @@
+
+# Packages -----------------------------------------------------------------
+require(EpiNow)
+require(NCoVUtils)
+require(furrr)
+require(future)
+require(dplyr)
+require(tidyr)
+require(magrittr)
+
+
+
+# Get cases ---------------------------------------------------------------
+
+NCoVUtils::reset_cache()
+
+cases <- NCoVUtils::get_uk_nhs_region_cases()
+
+cases <- cases %>% 
+  dplyr::rename(local = cases) %>% 
+  dplyr::mutate(imported = 0) %>% 
+  tidyr::gather(key = "import_status", value = "cases", local, imported)
+
+# Get linelist ------------------------------------------------------------
+
+linelist <-  NCoVUtils::get_international_linelist()
+
+# Set up cores -----------------------------------------------------
+
+future::plan("multiprocess", workers = future::availableCores())
+
+data.table::setDTthreads(threads = 1)
+
+# Run pipeline ----------------------------------------------------
+
+EpiNow::regional_rt_pipeline(
+  cases = cases, 
+  linelist = linelist, 
+  regional_delay = FALSE,
+  target_folder = "_posts/national/united-kingdom/nowcast/regional", 
+  merge_onsets = FALSE,
+  horizon = 0
+)
+
+
+# Summarise results -------------------------------------------------------
+
+EpiNow::regional_summary(results_dir = "_posts/national/united-kingdom/nowcast/regional", 
+                         summary_dir = "_posts/national/united-kingdom/nowcast/regional-summary",
+                         target_date = "latest",
+                         region_scale = "Region")
