a23ba2978bfdcbbda98d424b84d417713ed2dbd9
seabbs <s.e.abbott12@gmail.com>
2020-04-12 18:01:35 +0000

    Updated website
    Former-commit-id: 1ee85e895b39c901fdbe246adfc68baa3adf6f6b

--- utils/update_posts.R
+++ utils/update_posts.R
@@ -2,11 +2,14 @@ require(stringr)
 require(magrittr)
 require(future)
 require(furrr)
+require(purrr)
 
 posts <- c("_posts/global" , list.dirs("_posts/national", recursive = FALSE))
 
 
 future::plan("multiprocess")
+
+safe_render <- purrr::safely(rmarkdown::render)
 
 rendered_output <- furrr::future_map(posts, 
             function(post) {
@@ -14,5 +17,5 @@ rendered_output <- furrr::future_map(posts,
                 dplyr::last()
               file.copy("library.bib", file.path(post), overwrite = TRUE)
               
-              rmarkdown::render(file.path(post, paste0(post_name, ".Rmd")), quiet = FALSE)
+              safe_render(file.path(post, paste0(post_name, ".Rmd")), quiet = FALSE)
             }, .progress = TRUE)
