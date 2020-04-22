f77f45f92598918f417593f2ca95531e610aa1ef
seabbs <s.e.abbott12@gmail.com>
2020-04-22 14:26:30 +0000

    fix Rt csv to 1 dp
    Former-commit-id: a7a43515cec19877b3587b79f2f582393d4b8c72

--- utils/save_summary_csv.R
+++ utils/save_summary_csv.R
@@ -13,8 +13,9 @@ save_summary_csv <- function(results_dir = NULL, summary_dir = NULL,
   
   
   rt <- data.table::as.data.table(timeseries$rt)[rt_type %in% "nowcast" & type %in% "nowcast", 
-                      .(region, date = date - lubridate::days(incubation_shift), median,
-                        lower_90 = bottom, upper_90 = top, lower_50 = lower, upper_50 = upper)]
+                      .(region, date = date - lubridate::days(incubation_shift), median = round(median, 1),
+                        lower_90 = round(bottom, 1), upper_90 = round(top, 1),
+                        lower_50 = round(lower, 1), upper_50 = round(upper, 1))]
   
   data.table::setnames(rt, "region", type)
   
