35dc92ce30fe694903aeef35ff5c16665f0ccb9c
seabbs <s.e.abbott12@gmail.com>
2020-04-25 16:10:43 +0000

    split bash control for modularity
    Former-commit-id: 469f17a9703a77ba01a9233b99c5a4fd1734cfbe

---
+++ bin/update_deps.sh
@@ -0,0 +1,4 @@
+#!bin/bash
+
+
+Rscript -e "devtools::install_dev_deps()"
--- bin/update_website.sh
+++ bin/update_website.sh
@@ -31,11 +31,3 @@ Rscript -e "rmarkdown::render_site()"
 ## Clean up nowcast folders
 Rscript utils/clean_built_site.R
 
-## Update the submodule gits
-bash bin/update_submodules.sh
-
-## Update the git repo with new results
-git add --all && git commit -m "Updated website" && git push
-
-## Deploy the website to gh-pages
-bash bin/deploy_website.sh
