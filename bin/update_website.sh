f473c5afdac1e3f7727994a6fd5bb9ecf28d89ff
seabbs <s.e.abbott12@gmail.com>
2020-05-24 17:42:28 +0000

    added paper with figures

--- bin/update_website.sh
+++ bin/update_website.sh
@@ -10,9 +10,14 @@ Rscript utils/update_paper.R
 ## Update all posts
 Rscript utils/update_posts.R
 
+## Copy paper figures into root directory
+cp -r _paper/figures figures/
+
 ## Update the website
 Rscript -e "rmarkdown::render_site()"
 
 ## Clean up nowcast folders
 Rscript utils/clean_built_site.R
 
+## Remove paper figures from root
+rm -r -f figures
