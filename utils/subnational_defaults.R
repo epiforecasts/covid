12688af901a97fb641b63f9b20bc3660e5b76b85
seabbs <s.e.abbott12@gmail.com>
2020-08-06 16:42:31 +0000

    update template for subnational estimates to pull from default script
    Former-commit-id: 04df5a61bd6bc0a3448911104f1f09118b20493c

---
+++ utils/subnational_defaults.R
@@ -0,0 +1,50 @@
+
+# Knitr options -----------------------------------------------------------
+
+knitr::opts_chunk$set(echo = FALSE, eval = TRUE,
+                      message = FALSE,
+                      warning = FALSE,
+                      fig.height = 12,
+                      fig.width = 12,
+                      dpi = 300)
+
+# Load packages -----------------------------------------------------------
+
+library(EpiNow2)
+library(dplyr)
+library(magrittr)
+library(rnaturalearth)
+library(sf)
+library(rmarkdown)
+library(here)
+
+
+
+# Load summary data -------------------------------------------------------
+
+summary_table <- data.table::fread(here::here("covid-rt-estimates", "subnational", 
+                                              folder, "cases", 
+                                              "summary", "summary_table.csv"))
+latest_date <- readRDS(here::here("covid-rt-estimates", "subnational", folder,
+                                  "cases", "summary", "latest_date.rds"))
+
+
+
+# Region report control parameters ----------------------------------------
+
+summary_tables <- 0
+summary_figures <- 1
+title_depth <- 3
+index <- 1
+region <- "United Kingdom"
+region_path <- "covid-rt-estimates/national/cases/national"
+show_title <- FALSE
+report_forecast <- TRUE
+interactive <- TRUE
+
+
+
+# Region summary control parameters ---------------------------------------
+
+standalone <- FALSE
+
