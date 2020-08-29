752d2411ce8f1be4a7911e7d90bcfbd728db5115
seabbs <s.e.abbott12@gmail.com>
2020-08-29 02:10:05 +0000

    updat master
    Former-commit-id: 88e666cdea9cbc8a37f5ce45ba4f758d9354b5b9

--- bin/update_github.sh
+++ bin/update_github.sh
@@ -5,10 +5,11 @@ git add --all && git commit -m "Updated website" && git push
 
 ## Deploy the website to gh-pages
 cp -r docs ../docs
-git checkout --orphan -b  gh-pages
+git checkout --orphan gh-pages
 rm -f -r *
 cp -r ../docs/. .
 git add --all
 git commit -m "deploy site"
 git push -f origin gh-pages:gh-pages
+git checkout master
 git branch -D gh-pages
