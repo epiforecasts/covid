e69ea86785945b9c0a1a1760720c09784c41b546
seabbs <s.e.abbott12@gmail.com>
2020-08-29 15:51:50 +0000

    Revert "deploy site"
    This reverts commit 554cef100ddca023a122563907f47a78da2a1432 [formerly dc4714780590407a8938573fdc4973e11c1d282e].
    
    
    Former-commit-id: 92b134f2f817bd36df9f7f983c3198d2605afe83

---
+++ utils/subnational_defaults.R
@@ -0,0 +1,48 @@
+
+# Knitr options -----------------------------------------------------------
+
+knitr::opts_chunk$set(echo = FALSE, eval = TRUE,
+                      message = FALSE,
+                      warning = FALSE,
+                      fig.height = 12,
+                      fig.width = 12,
+                      dpi = 300)
+
+# Load packages -----------------------------------------------------------
+
+library(EpiNow2)
+library(covidregionaldata)
+library(dplyr)
+library(magrittr)
+library(rnaturalearth)
+library(sf)
+library(rmarkdown)
+library(here)
+library(stringr)
+library(ggplot2)
+
+# Load summary data -------------------------------------------------------
+
+summary_table <- data.table::fread(here::here("covid-rt-estimates", "subnational", 
+                                              folder, "cases", 
+                                              "summary", "summary_table.csv"))
+latest_date <- readRDS(here::here("covid-rt-estimates", "subnational", folder,
+                                  "cases", "summary", "latest_date.rds"))
+
+
+
+# Region report control parameters ----------------------------------------
+
+summary_tables <- 0
+summary_figures <- 1
+title_depth <- 3
+index <- 1
+region_path <- "covid-rt-estimates/national/cases/national"
+show_title <- FALSE
+report_forecast <- TRUE
+interactive <- TRUE
+
+# Region summary control parameters ---------------------------------------
+
+standalone <- FALSE
+
