8158a3c7efb8810624419c5c32474085d8fe6196
seabbs <s.e.abbott12@gmail.com>
2020-09-02 17:51:19 +0000

    deploy site
    Former-commit-id: 50c39da93c03acc65e466de1392ce42b7649c257

--- utils/update_paper.R
+++
@@ -1,8 +0,0 @@
-require(rmarkdown)
-
-
-## Copy master bib into resources folder
-file.copy("library.bib", "_paper/resources/", overwrite = TRUE)
-
-## Render into word and pdf
-rmarkdown::render("_paper/paper.Rmd", output_format = c("pdf_document", "word_document"))
