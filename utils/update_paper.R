67abe571790ede3c10025e0dab711fdc72c1d401
seabbs <s.e.abbott12@gmail.com>
2020-09-02 17:57:12 +0000

    Revert "deploy site"
    This reverts commit 9713fd823c8ecd5a60bc342c8581224b4dab78fc [formerly 50c39da93c03acc65e466de1392ce42b7649c257].
    
    
    Former-commit-id: 0039f11c5fb724c30d0b5130c4872e2398ad76e8

---
+++ utils/update_paper.R
@@ -0,0 +1,8 @@
+require(rmarkdown)
+
+
+## Copy master bib into resources folder
+file.copy("library.bib", "_paper/resources/", overwrite = TRUE)
+
+## Render into word and pdf
+rmarkdown::render("_paper/paper.Rmd", output_format = c("pdf_document", "word_document"))
