1e0fb0eecbd2b31518e97b25402d0207496b402d
seabbs <s.e.abbott12@gmail.com>
2020-09-04 12:46:13 +0000

    add support for regional estimates
    Former-commit-id: b4cce440e7d93917286443e4ad2097a1c037cb7e

--- utils/join_RtD3_data.R
+++ utils/join_RtD3_data.R
@@ -2,20 +2,23 @@ library(purrr, quietly = TRUE)
 library(RtD3, quietly = TRUE)
 library(data.table, quietly = TRUE)
 
-get_national_RtD3_data <- function(rtData, country) {
+join_RtD3_data <- function(rtData, country,
+                           base_url = 'https://raw.githubusercontent.com/epiforecasts/covid-rt-estimates/master/national/cases/summary',
+                           region_id = "country") {
 
-  # Define the base URL/file path for the estimates
-  base_url <- 'https://raw.githubusercontent.com/epiforecasts/covid-rt-estimates/master/national/'
-  
   # Read in each summary folder
-  rtData_national <- RtD3::readInEpiNow2(path = paste0(base_url, "cases/summary"),
-                                                        region_var = "country")
+  rtData_national <- RtD3::readInEpiNow2(path = base_url, region_var = region_id)
   
   # Make sure all regions have the same id
   rtData_national$summaryData <- rtData_national$summaryData[, region := Region]
   
+  ## Make country filter all regions if missing
+  if (missing(country)) {
+    country <- unique(rtData_national[[1]]$region)
+  }
+  
   # Join the subregional and national estimates together.
-  rtData$Cases <- purrr::map2(rtData$Cases, rtData_national, 
+  rtData <- purrr::map2(rtData, rtData_national, 
                         ~ data.table::rbindlist(list(
                           .x, 
                           .y[region %in% country]),
