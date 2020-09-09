5b5fb1c9e6102005244fb0ce3f0645d76914a96a
seabbs <s.e.abbott12@gmail.com>
2020-09-09 15:09:03 +0000

    add secondary update run and logging of failed renders

--- utils/update_posts.R
+++ utils/update_posts.R
@@ -27,7 +27,7 @@ render_by_path <-  function(post) {
   if (!is.null(tmp[[2]])) {
     warning(post_name, ": ", tmp[[2]])
   }
-  return(tmp[[2]])
+  return(tmp)
 }
 
 ## Render posts in parallel
@@ -35,19 +35,18 @@ failed_to_render <- furrr::future_map(posts, render_by_path, .progress = TRUE)
 failed_to_render <- purrr::transpose(failed_to_render)
 
 ## Try to render failed posts again (in serial)
-secondary_render_failure <- purrr::map(posts[!is.null(failed_to_render[[2]])],
+secondary_render_failure <- purrr::map(posts[purrr::map_lgl(failed_to_render[[2]], ~ !is.null(.))],
                                        render_by_path)
 
 ## Manipulate errors and store in a nice format
 secondary_render_failure <- purrr::transpose(secondary_render_failure)
 secondary_render_failure <- secondary_render_failure[[2]]
-names(secondary_render_failure) <- posts[!is.null(failed_to_render[[2]])]
 secondary_render_failure <- purrr::compact(secondary_render_failure)
 
-render_failure <- data.table::data.table(page = names(secondary_render_failure), 
-                                         error = secondary_render_failure)
+render_failure <- data.table::data.table(page = posts[purrr::map_lgl(failed_to_render[[2]], ~ !is.null(.))], 
+                                         error = as.character(secondary_render_failure))
 ## Save errors
-data.table::fwrite(render_failure, "logs/render_failure.csv")
+data.table::fwrite(render_failure, here::here("logs", "render_failure.csv"))
 
 ## Copy library into root to check for issues
 file.copy("_posts/global/library.bib", ".", overwrite = FALSE)
