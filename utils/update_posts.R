837c71480d3315acdac48cc5944f2b1ca39496aa
seabbs <s.e.abbott12@gmail.com>
2020-09-09 15:29:28 +0000

    tweak failure logging

--- utils/update_posts.R
+++ utils/update_posts.R
@@ -41,12 +41,18 @@ secondary_render_failure <- purrr::map(posts[purrr::map_lgl(failed_to_render[[2]
 ## Manipulate errors and store in a nice format
 secondary_render_failure <- purrr::transpose(secondary_render_failure)
 secondary_render_failure <- secondary_render_failure[[2]]
+names(secondary_render_failure) <- posts[purrr::map_lgl(failed_to_render[[2]], ~ !is.null(.))]
 secondary_render_failure <- purrr::compact(secondary_render_failure)
+secondary_render_failure <- as.character(secondary_render_failure)
 
-render_failure <- data.table::data.table(page = posts[purrr::map_lgl(failed_to_render[[2]], ~ !is.null(.))], 
-                                         error = as.character(secondary_render_failure))
+render_failure <- data.table::data.table(page = names(secondary_render_failure), 
+                                         error = secondary_render_failure)
 ## Save errors
 data.table::fwrite(render_failure, here::here("logs", "render_failure.csv"))
+
+
+## Print to terminal the failed renders
+print(render_failure)
 
 ## Copy library into root to check for issues
 file.copy("_posts/global/library.bib", ".", overwrite = FALSE)
