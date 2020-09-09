0a73a8512ab7f9db379c72d09b83ffbf3bb4ca68
seabbs <s.e.abbott12@gmail.com>
2020-09-09 16:18:22 +0000

    Updated website
    Former-commit-id: c22a6fb888bcdf1c8986975178f9f9ea9d43dd69

--- utils/update_posts.R
+++ utils/update_posts.R
@@ -41,12 +41,13 @@ secondary_render_failure <- purrr::map(posts[purrr::map_lgl(failed_to_render[[2]
 ## Manipulate errors and store in a nice format
 secondary_render_failure <- purrr::transpose(secondary_render_failure)
 secondary_render_failure <- secondary_render_failure[[2]]
-names(secondary_render_failure) <- posts[purrr::map_lgl(failed_to_render[[2]], ~ !is.null(.))]
-secondary_render_failure <- purrr::compact(secondary_render_failure)
-secondary_render_failure <- as.character(secondary_render_failure)
+secondary_render_failure_cmp <- purrr::compact(secondary_render_failure)
+secondary_render_failure_cmp <- as.character(secondary_render_failure_cmp)
+tmp <- posts[purrr::map_lgl(failed_to_render[[2]], ~ !is.null(.))]
+names(secondary_render_failure_cmp) <- tmp[purrr::map_lgl(secondary_render_failure, ~ !is.null(.))]
 
-render_failure <- data.table::data.table(page = names(secondary_render_failure), 
-                                         error = secondary_render_failure)
+render_failure <- data.table::data.table(page = names(secondary_render_failure_cmp), 
+                                         error = secondary_render_failure_cmp)
 ## Save errors
 data.table::fwrite(render_failure, here::here("logs", "render_failure.csv"))
 
@@ -56,3 +57,7 @@ print(render_failure)
 
 ## Copy library into root to check for issues
 file.copy("_posts/global/library.bib", ".", overwrite = FALSE)
+
+
+## Clean up an misplaced html files
+list.files()
