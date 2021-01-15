05d535b804dcd94ab37f5c0df0bdbd9e37c8e65b
seabbs <s.e.abbott12@gmail.com>
2021-01-15 14:47:53 +0000

    fix page delete controls

--- utils/write_subnational_estimates.R
+++ utils/write_subnational_estimates.R
@@ -1,5 +1,5 @@
-countries <- c("Italy", "United Kingdom", "United States", "Germany",
-               "Brazil", "India", "Colombia",
-               "Canada", "Belgium")
+countries <- c("italy", "united-kingdom", "united-states", "germany",
+               "brazil", "india", "colombia",
+               "canada", "belgium")
 
 saveRDS(countries, here::here("data", "subnational_estimates.rds"))
